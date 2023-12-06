41ffcf4f47a7ce205cd54bcb736d979d11cc6eec
Rémy Maucourt <remy.maucourt@yahoo.fr>
2023-12-06 13:01:20 +0100

    validate uniqueness of username

--- messenger/forms.py
+++ messenger/forms.py
@@ -1,5 +1,12 @@
 from django import forms
-
+from django.core.exceptions import ValidationError
+from .models import ChatUser
 
 class UserNameForm(forms.Form):
     username = forms.CharField(label="Choisir un pseudo", max_length=30)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if ChatUser.objects.filter(username=username).exists():
+            raise ValidationError("username already exists")
+        return username
--- messenger/models.py
+++ messenger/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class ChatUser(models.Model):
-    username = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
