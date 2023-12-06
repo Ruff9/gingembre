a4188684891cfcc7248b23318bd15c9da78458ab
Rémy Maucourt <remy.maucourt@yahoo.fr>
2023-12-06 14:25:50 +0100

    serve static files in production

--- gingembre/asgi.py
+++ gingembre/asgi.py
@@ -14,12 +14,14 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
+from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gingembre.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
+
+# use of cling for https://doc.scalingo.com/languages/python/django/start#static-file-serving
+django_asgi_app = Cling(get_asgi_application())
 
 import messenger.routing
 from messenger.routing import websocket_urlpatterns
