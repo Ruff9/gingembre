dc2e51c8a534288422859c78e134f072def343ce
Rémy Maucourt <remy.maucourt@yahoo.fr>
2023-12-06 14:09:53 +0100

    fix bug deployment

--- gingembre/asgi.py
+++ gingembre/asgi.py
@@ -15,7 +15,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from messenger.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gingembre.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
@@ -23,6 +22,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gingembre.settings')
 django_asgi_app = get_asgi_application()
 
 import messenger.routing
+from messenger.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
