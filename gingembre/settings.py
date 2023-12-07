78dd46a53cb3d1ce9527383b1599f30be35e3a65
Rémy Maucourt <remy.maucourt@yahoo.fr>
2023-12-07 19:17:16 +0100

    fix bug deployment 10

--- gingembre/settings.py
+++ gingembre/settings.py
@@ -19,7 +19,7 @@ env = environ.Env(
 )
 
 # Set the project base directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
