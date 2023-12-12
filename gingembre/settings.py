12dfb5a6a2596a0f4d97c331fd6e7645ab785517
Rémy Maucourt <remy.maucourt@yahoo.fr>
2023-12-12 16:07:08 +0100

    clean settings

--- gingembre/settings.py
+++ gingembre/settings.py
@@ -12,40 +12,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 
-# Setup google app engine
-# import io
-
 import environ
 from google.cloud import secretmanager
 
 env = environ.Env(DEBUG=(bool, False))
 
-# Set the project base directory
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-# Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-
-# Setup google app engine
-# env_file = os.path.join(BASE_DIR, ".env")
-
-# if os.path.isfile(env_file):
-#     # Use a local secret file, if provided
-#     env.read_env(env_file)
-
-# elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-#     # Pull secrets from Secret Manager
-#     project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-
-#     client = secretmanager.SecretManagerServiceClient()
-#     settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
-#     name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
-#     payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
-
-#     env.read_env(io.StringIO(payload))
-# else:
-#     raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
 
 
 # False if not in os.environ because of casting above
@@ -63,12 +37,6 @@ if DEBUG:
     ALLOWED_HOSTS = ['*']
 else:
     ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-
-# Setup google app engine
-# if DEBUG:
-#     ALLOWED_HOSTS = ['*']
-# else:
-#     ALLOWED_HOSTS = ['annular-ocean-407615.ew.r.appspot.com']
 
 
 # Application definition
