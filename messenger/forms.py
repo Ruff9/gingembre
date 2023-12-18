ba1d02c3b0b30563004264c60ec3556ce67f0a15
Rémy Maucourt <remy.maucourt@yahoo.fr>
2023-12-18 17:55:26 +0100

    add error message in translation file

--- messenger/forms.py
+++ messenger/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
 from .models import ChatUser
 
 class UserNameForm(forms.Form):
-    username = forms.CharField(label="Choisir un pseudo", max_length=30)
+    username = forms.CharField(max_length=30)
 
     def clean_username(self):
         username = self.cleaned_data['username']
         if ChatUser.objects.filter(username=username).exists():
-            raise ValidationError("Ce pseudo est déjà pris.")
+            raise ValidationError(_("TakenUserName"))
         return username
