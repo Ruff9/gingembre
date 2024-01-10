c7de5dd8282dd1389373b8eb15bd14bba7b9fbdc
Rémy Maucourt <remy.maucourt@yahoo.fr>
2024-01-10 17:29:42 +0100

    update manifest

---
+++ messenger/tests/test_notification.py
@@ -0,0 +1,8 @@
+# workflow notification :
+
+# -  1 message est créé, 1 notification aussi, a l'état "non lu"
+# - quand 1 user ouvre la conversation qui contient le message, la notification passe en état "lu"
+# - On affiche les notifications non lues, sur les conversations et sur le retour à l'accueil
+
+# Notifications enregistrees en base, avec l'ID du message concerné
+# (plus tard, pt etre ajouté dans l'objet en base le texte / début du message)
